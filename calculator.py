693847cf9c9a8c101b936f446452a6b587d74b1f
Ofelia <oer.rodriguez@gmail.com>
2021-01-20 05:01:31 +0000

    Added checks for unintended user inputs

--- calculator.py
+++ calculator.py
@@ -8,36 +8,43 @@ def calculate ():
 
     while True:
 
-        input_string = input('Enter your equation > ')
-        tokens = input_string.split(' ')
+        try: 
 
-        if input_string == 'q':
-            print('You will exit.')
-            break
-        
-        elif tokens[0] == '+':
-            print(f'{add(float(tokens[1]), float(tokens[2]))}')
+            input_string = input('Enter your equation > ')
+            tokens = input_string.split(' ')
 
-        elif tokens[0] == '-':
-            print(f'{subtract(float(tokens[1]), float(tokens[2]))}')
+            if input_string == 'q':
+                print('You will exit.')
+                break
+            
+            elif tokens[0] == '+':
+                print(f'{add(float(tokens[1]), float(tokens[2]))}')
 
-        elif tokens[0] == '*':
-            print(f'{multiply(float(tokens[1]), float(tokens[2]))}')
-        
-        elif tokens[0] == '/':
-            print(f'{divide(float(tokens[1]), float(tokens[2]))}')
+            elif tokens[0] == '-':
+                print(f'{subtract(float(tokens[1]), float(tokens[2]))}')
 
-        elif tokens[0] == 'square':
-            print(f'{square(float(tokens[1]))}')
-        
-        elif tokens[0] == 'cube':
-            print(f'{cube(float(tokens[1]))}')
-        
-        elif tokens[0] == 'pow':
-            print(f'{power(float(tokens[1]), float(tokens[2]))}')
+            elif tokens[0] == '*':
+                print(f'{multiply(float(tokens[1]), float(tokens[2]))}')
+            
+            elif tokens[0] == '/':
+                print(f'{divide(float(tokens[1]), float(tokens[2]))}')
 
-        elif tokens[0] == 'mod':
-            print(f'{mod(float(tokens[1]), float(tokens[2]))}')
-        
+            elif tokens[0] == 'square':
+                print(f'{square(float(tokens[1]))}')
+            
+            elif tokens[0] == 'cube':
+                print(f'{cube(float(tokens[1]))}')
+            
+            elif tokens[0] == 'pow':
+                print(f'{power(float(tokens[1]), float(tokens[2]))}')
+
+            elif tokens[0] == 'mod':
+                print(f'{mod(float(tokens[1]), float(tokens[2]))}')
+            
+            else:
+                print('Oh no! Please enter a correct input.')
+        except:
+                print('Please enter the correct number of commands.')
+
 calculate()
 
